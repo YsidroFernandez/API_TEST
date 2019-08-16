f55eebddefe5259eb9a2922da53601e7325f0087
ysidrofernandez <ysidrofernandez3012@gmail.com>
2019-08-16 19:53:49 -0400

    update changes in structure projects

--- config.js
+++ config.js
@@ -3,3 +3,9 @@ module.exports = {
     db: process.env.MONGODB || 'mongodb://localhost:27017/products',
     SECRET_TOKEN: '123YJFStokens'
 }
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+//   }
+  
+//   const DB_URI = process.env.DB_URI
+//   const PORT = process.env.PORT
