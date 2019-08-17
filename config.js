daedfb5a9a7964bb4acd39447587ec375ae07bce
ysidrofernandez <ysidrofernandez3012@gmail.com>
2019-08-17 10:13:34 -0400

    test

--- config.js
+++ config.js
@@ -6,6 +6,5 @@ module.exports = {
 // if (process.env.NODE_ENV !== 'production') {
 //     require('dotenv').config()
 //   }
-  
 //   const DB_URI = process.env.DB_URI
 //   const PORT = process.env.PORT
